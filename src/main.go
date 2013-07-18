76baf3cefd4f47702d126af135d0642590ec6d0c
grapswiz <ineeza@gmail.com>
2013-07-18 11:51:27 +0900

    Save urls into Rss kind

--- src/main.go
+++ src/main.go
@@ -11,8 +11,8 @@ import (
 
 func init() {
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/createsamplerss", sampleHandler)
-	http.HandleFunc("/auth", authHandler)
+	http.HandleFunc("/createsamplerss/", sampleHandler)
+	http.HandleFunc("/auth/", authHandler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request)  {
@@ -20,13 +20,10 @@ func handler(w http.ResponseWriter, r *http.Request)  {
 }
 
 func sampleHandler(w http.ResponseWriter, r *http.Request) {
-	var urls []string
-	urls[0] = "http://blog.memoto.com"
-	urls[1] = "http://blog.alexmaccaw.com"
 	r1 := rss.Rss{
 		Created: time.Now(),
 		Updated: time.Now(),
-		Urls: urls,
+		Urls: []string{"http://blog.memoto.com", "http://blog.alexmaccaw.com"},
 	}
 	r2, _ := r1.Save(appengine.NewContext(r))
 	fmt.Fprint(w, r2.Id)
